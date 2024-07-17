eb8380734a38803b69f08d55ba3d67a4006e3369
jahongir <jahongirqahharov19@gmail.com>
2024-07-17 17:56:51 +0500

    Add files via upload

---
+++ Lesson.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Lesson.css';
+
+function Lesson() {
+  return (
+    <div className='Spoon'>
+        <h1>Promoting your business and services with</h1>
+        <b>Propl :</b> <p> A responsive, multi-page business <br/></p>
+        <p> template with everything you need propel your <br/> business.</p>
+        <div className='box'>
+            <div className='text'>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkhb7P-CstHJQKFHKutEhdwQchbBKmVLXnLg&s' /> <br/>  <br/> <br/> 
+                <b className='b'>Pro ad constituto</b>
+                <p className='p'>Ne usu illud albucius abhor reant, <br/> partiendo scriptorem mel ne.</p>
+            </div>
+            <div className='text'>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUgbiLEm9iRz6raPtEMl0z1Ks9D5kuXHUeWQ&s' /> <br/>  <br/> <br/> 
+                <b className='b'>Eam commodo</b>
+                <p className='p'>Fuisset intellegat delicatissimi <br/> ex mea. Cu probo integre nec.</p>
+            </div>
+            <div className='text'>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaWc_0TkYtnminyvGWI3KQOuuLtimfplHoxQ&s' /> <br/>  <br/> <br/>     
+                <b className='b'>Reformidans eis</b>
+                <p className='p'>Facer nostro causae ei sed. Vix <br/> ex quidam detraxit, vis ad <br/> nostro laoreet.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Lesson;
+
